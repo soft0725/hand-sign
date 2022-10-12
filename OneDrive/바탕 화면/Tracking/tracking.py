b252ffa20bfaac81a6b1013a1c0c558bb17d38fe
soft0725 <a01021146076@gmail.com>
2022-10-12 19:46:37 +0900

    주먹과 좋아요 인식

--- OneDrive/바탕 화면/Tracking/tracking.py
+++ OneDrive/바탕 화면/Tracking/tracking.py
@@ -3,13 +3,29 @@ import mediapipe as mp # 손가락 인식
 import math
 from PIL import ImageFont
 
+def get_dis(dis):
+    return float(dis * 500)
+
+def print_text(str):
+    cv2.putText(
+    image, text = str, org=(10, 30),
+    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+    color=255, thickness=2)
+
+def print_num(dis_1, dis_2, dis_3, dis_4, dis_5):
+    cv2.putText(
+    image, text = '%d  %d  %d  %d  %d' % (dis_1, dis_2, dis_3, dis_4, dis_5) , org=(10, 30),
+    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+    color=255, thickness=2)
+
+
 mp_drawing = mp.solutions.drawing_utils # 웹캠 영상에서 뼈 마디를 인식함.
 mp_hands = mp.solutions.hands # 동일하다 
 
 cap = cv2.VideoCapture(0) # 웹 캠을 열어준다.
 
 with mp_hands.Hands(
-    max_num_hands=1, # 최대 손 인식 갯수
+    max_num_hands=2, # 최대 손 인식 갯수
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
 
@@ -29,59 +45,70 @@ with mp_hands.Hands(
 
         if result.multi_hand_landmarks: # 이미지가 인식이 되어서 True가 된다면 
             for hand_landmarks in result.multi_hand_landmarks:
-
-                thumb_tip = hand_landmarks.landmark[4] # 엄지 
-
-                index_tip = hand_landmarks.landmark[8] # 검지  
-
-                middle_finger_tip = hand_landmarks.landmark[12] # 중지 
-
-                ring_finger_tip = hand_landmarks.landmark[16] # 약지 
-                ring_finger_pip = hand_landmarks.landmark[14] # 약지-2
-
-                pinky_tip = hand_landmarks.landmark[20] # 소지 
-                pinky_dip = hand_landmarks.landmark[19] # 소지-1
                 
+                # top
+                thumb_tip = hand_landmarks.landmark[4] 
+                index_tip = hand_landmarks.landmark[8]
+                middle_tip = hand_landmarks.landmark[12]  
+                ring_tip = hand_landmarks.landmark[16] 
+                pinky_tip = hand_landmarks.landmark[20] 
+
+                # 2 
+                thumb_ip = hand_landmarks.landmark[3]
+                index_dip = hand_landmarks.landmark[7]
+                middle_dip = hand_landmarks.landmark[11]
+                ring_dip = hand_landmarks.landmark[15]
+                pinky_dip = hand_landmarks.landmark[19]
+
+                # 3
+                index_pip = hand_landmarks.landmark[6]
+                middle_pip = hand_landmarks.landmark[10]
+                ring_pip = hand_landmarks.landmark[14]
+                pinky_pip = hand_landmarks.landmark[18]
+
+                # 4 
+                index_mcp = hand_landmarks.landmark[5]
+                middle_mcp = hand_landmarks.landmark[9]
+                ring_mcp = hand_landmarks.landmark[13]
+                pinky_mcp = hand_landmarks.landmark[17]
+                # 2 랑 4 의 거리가 작아야 주먹이된다.
+
                 dif1 = math.sqrt(((thumb_tip.x-index_tip.x)**2) + ((thumb_tip.y-index_tip.y)**2))
                 # 엄지와 검지의 거리 
-                dif2 = math.sqrt(((index_tip.x-middle_finger_tip.x)**2) + ((index_tip.y-middle_finger_tip.y)**2))
+                dif2 = math.sqrt(((index_tip.x-middle_tip.x)**2) + ((index_tip.y-middle_tip.y)**2))
                 # 검지와 중지의 거리 
-                dif3 = math.sqrt(((middle_finger_tip.x-ring_finger_tip.x)**2) + ((middle_finger_tip.y-ring_finger_tip.y)**2))
+                dif3 = math.sqrt(((middle_tip.x-ring_tip.x)**2) + ((middle_tip.y-ring_tip.y)**2))
                 # 중지와 약지의 거리  
-                dif4 = math.sqrt(((ring_finger_pip.x-pinky_dip.x)**2) + ((ring_finger_pip.y-pinky_dip.y)**2))
+                dif4 = math.sqrt(((ring_pip.x-pinky_dip.x)**2) + ((ring_pip.y-pinky_dip.y)**2))
                 # 약지와 소지의 거리  
 
-                diff_result1 = float(dif1 * 500)
-                diff_result2 = float(dif2 * 500)
-                diff_result3 = float(dif3 * 500)
-                diff_result4 = float(dif4 * 500)
-                # 현재 가장 적절한 값은 500
+                rock1 = (((index_dip.x-index_mcp.x)**2) + ((index_dip.y-index_mcp.y)**2))
+                rock2 = (((middle_dip.x-middle_mcp.x)**2) + ((middle_dip.y-middle_mcp.y)**2))
+                rock3 = (((ring_dip.x-ring_mcp.x)**2) + ((ring_dip.y-ring_mcp.y)**2))
+                rock4 = (((pinky_dip.x-pinky_mcp.x)**2) + ((pinky_dip.y-pinky_mcp.y)**2))
+                rock5 = (((thumb_tip.x-index_pip.x)**2) + ((thumb_tip.y-index_pip.y)**2))
 
-                if diff_result1 <= 15.6:
-                    cv2.putText(
-                    image, text = '4-8', org=(10, 30),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-                    color=255, thickness=2)
-                elif diff_result2 <= 25.9:
-                    cv2.putText(
-                    image, text = '8-12', org=(10, 30),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-                    color=255, thickness=2)
-                elif diff_result3 <= 21.2:
-                    cv2.putText(
-                    image, text = '12-16', org=(10, 30),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-                    color=255, thickness=2)
-                elif diff_result4 <= 17.3:
-                    cv2.putText(
-                    image, text = '16-20', org=(10, 30),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-                    color=255, thickness=2)
-                else :
-                    cv2.putText(
-                    image, text = '%d  %d  %d  %d' % (diff_result1, diff_result2, diff_result3, diff_result4) , org=(10, 30),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-                    color=255, thickness=2)
+                dis_1 = get_dis(rock1)
+                dis_2 = get_dis(rock2)
+                dis_3 = get_dis(rock3)
+                dis_4 = get_dis(rock4)
+                dis_5 = get_dis(rock5)
+
+                # if dis_1 <= 15.6:
+                #     print_text('4-8')
+                # elif dis_2 <= 25.9:
+                #     print_text('8-12')
+                # elif dis_3 <= 21.2:
+                #     print_text('12-16')
+                # elif dis_4 <= 17.3:
+                #     print_text('16-20')
+
+                if dis_1 <= 2 and dis_2 <= 2 and dis_3 <= 2 and dis_4 <= 2 and dis_5 <= 2:
+                    print_text('rock')
+                elif dis_1 <= 2 and dis_2 <= 2 and dis_3 <= 2 and dis_4 <= 2 and dis_5 >= 2:
+                    print_text('good')
+                else:
+                    print_num(dis_1,dis_2,dis_3,dis_4,dis_5)
 
                 mp_drawing.draw_landmarks(
                     image,hand_landmarks, mp_hands.HAND_CONNECTIONS)
