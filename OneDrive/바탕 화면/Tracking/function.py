3e78205175c9cb7d50a85a9d5af304cd1a02a0d0
soft0725 <a01021146076@gmail.com>
2022-10-13 13:33:41 +0900

    손가락 욕을 인식하는 코드 추가

---
+++ OneDrive/바탕 화면/Tracking/function.py
@@ -0,0 +1,19 @@
+import cv2
+import math
+
+def get_dis(x1,x2,y1,y2):
+    value = math.sqrt(((x1-x2)**2) + ((y1-y2)**2))
+    result = value * 500
+    return result
+
+def motion(image, value):
+    cv2.putText(
+    image, text = value, org=(10, 30),
+    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+    color=255, thickness=2)
+
+def print_num(image, rock1, rock2, rock3, rock4, rock5):
+    cv2.putText(
+    image, text = '%d  %d  %d  %d  %d' % (rock1, rock2, rock3, rock4, rock5) , org=(10, 30),
+    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+    color=255, thickness=2)
--- OneDrive/바탕 화면/Tracking/tracking.py
+++ OneDrive/바탕 화면/Tracking/tracking.py
@@ -1,23 +1,10 @@
 import cv2 
 import mediapipe as mp # 손가락 인식
 import math
-from PIL import ImageFont
 
-def get_dis(dis):
-    return float(dis * 500)
+from function import motion, print_num
 
-def print_text(str):
-    cv2.putText(
-    image, text = str, org=(10, 30),
-    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-    color=255, thickness=2)
-
-def print_num(dis_1, dis_2, dis_3, dis_4, dis_5):
-    cv2.putText(
-    image, text = '%d  %d  %d  %d  %d' % (dis_1, dis_2, dis_3, dis_4, dis_5) , org=(10, 30),
-    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-    color=255, thickness=2)
-
+W = 500 # 가중치 
 
 mp_drawing = mp.solutions.drawing_utils # 웹캠 영상에서 뼈 마디를 인식함.
 mp_hands = mp.solutions.hands # 동일하다 
@@ -25,7 +12,7 @@ mp_hands = mp.solutions.hands # 동일하다
 cap = cv2.VideoCapture(0) # 웹 캠을 열어준다.
 
 with mp_hands.Hands(
-    max_num_hands=2, # 최대 손 인식 갯수
+    max_num_hands=1, # 최대 손 인식 갯수
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
 
@@ -73,42 +60,24 @@ with mp_hands.Hands(
                 pinky_mcp = hand_landmarks.landmark[17]
                 # 2 랑 4 의 거리가 작아야 주먹이된다.
 
-                dif1 = math.sqrt(((thumb_tip.x-index_tip.x)**2) + ((thumb_tip.y-index_tip.y)**2))
-                # 엄지와 검지의 거리 
-                dif2 = math.sqrt(((index_tip.x-middle_tip.x)**2) + ((index_tip.y-middle_tip.y)**2))
-                # 검지와 중지의 거리 
-                dif3 = math.sqrt(((middle_tip.x-ring_tip.x)**2) + ((middle_tip.y-ring_tip.y)**2))
-                # 중지와 약지의 거리  
-                dif4 = math.sqrt(((ring_pip.x-pinky_dip.x)**2) + ((ring_pip.y-pinky_dip.y)**2))
-                # 약지와 소지의 거리  
+                # 주먹 확인 코드 
+                rock5 = (((thumb_tip.x-index_pip.x)**2) + ((thumb_tip.y-index_pip.y)**2)) * W # 엄지 
+                rock1 = (((index_dip.x-index_mcp.x)**2) + ((index_dip.y-index_mcp.y)**2)) * W # 검지 
+                rock2 = (((middle_dip.x-middle_mcp.x)**2) + ((middle_dip.y-middle_mcp.y)**2)) * W # 중지
+                rock3 = (((ring_dip.x-ring_mcp.x)**2) + ((ring_dip.y-ring_mcp.y)**2)) * W # 약지 
+                rock4 = (((pinky_dip.x-pinky_mcp.x)**2) + ((pinky_dip.y-pinky_mcp.y)**2)) * W # 소지
 
-                rock1 = (((index_dip.x-index_mcp.x)**2) + ((index_dip.y-index_mcp.y)**2))
-                rock2 = (((middle_dip.x-middle_mcp.x)**2) + ((middle_dip.y-middle_mcp.y)**2))
-                rock3 = (((ring_dip.x-ring_mcp.x)**2) + ((ring_dip.y-ring_mcp.y)**2))
-                rock4 = (((pinky_dip.x-pinky_mcp.x)**2) + ((pinky_dip.y-pinky_mcp.y)**2))
-                rock5 = (((thumb_tip.x-index_pip.x)**2) + ((thumb_tip.y-index_pip.y)**2))
-
-                dis_1 = get_dis(rock1)
-                dis_2 = get_dis(rock2)
-                dis_3 = get_dis(rock3)
-                dis_4 = get_dis(rock4)
-                dis_5 = get_dis(rock5)
-
-                # if dis_1 <= 15.6:
-                #     print_text('4-8')
-                # elif dis_2 <= 25.9:
-                #     print_text('8-12')
-                # elif dis_3 <= 21.2:
-                #     print_text('12-16')
-                # elif dis_4 <= 17.3:
-                #     print_text('16-20')
-
-                if dis_1 <= 2 and dis_2 <= 2 and dis_3 <= 2 and dis_4 <= 2 and dis_5 <= 2:
-                    print_text('rock')
-                elif dis_1 <= 2 and dis_2 <= 2 and dis_3 <= 2 and dis_4 <= 2 and dis_5 >= 2:
-                    print_text('good')
+                if ((((rock1 <= 2) and (rock2 <= 2)) and ((rock3 <= 2) and (rock4 <= 2))) and (rock5 <= 2)):
+                    text = 'ROCK'
+                    motion(image, text)
+                elif ((((rock1 <= 2) and (rock2 <= 2)) and ((rock3 <= 2) and (rock4 <= 2))) and (rock5 >= 2)):
+                    text = 'GOOD'
+                    motion(image, text)
+                elif ((((rock1 <= 2) and (rock3 <= 2)) and ((rock4 <= 2) and (rock5 <= 2))) and (rock2 >= 2)):
+                    text = 'Bad word'
+                    motion(image, text)
                 else:
-                    print_num(dis_1,dis_2,dis_3,dis_4,dis_5)
+                    print_num(image, rock5, rock1, rock2, rock3, rock4)
 
                 mp_drawing.draw_landmarks(
                     image,hand_landmarks, mp_hands.HAND_CONNECTIONS)
