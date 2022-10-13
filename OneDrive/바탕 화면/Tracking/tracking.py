179d9167b3814f1ff1ab99606e989c586f8d3320
soft0725 <a01021146076@gmail.com>
2022-10-13 16:09:13 +0900

    손의 제스처를 이용한 거리 측정 1회 가능한 코드

--- OneDrive/바탕 화면/Tracking/tracking.py
+++ OneDrive/바탕 화면/Tracking/tracking.py
@@ -1,10 +1,14 @@
+from tkinter import E
 import cv2 
 import mediapipe as mp # 손가락 인식
-import math
 
-from function import motion, print_num
+from function import motion, print_num, get_dis
 
 W = 500 # 가중치 
+
+list = [] # 손이 이동한 거리를 저장하기 위한 리스트 
+dis_temp = [0]
+value = 0 
 
 mp_drawing = mp.solutions.drawing_utils # 웹캠 영상에서 뼈 마디를 인식함.
 mp_hands = mp.solutions.hands # 동일하다 
@@ -32,7 +36,7 @@ with mp_hands.Hands(
 
         if result.multi_hand_landmarks: # 이미지가 인식이 되어서 True가 된다면 
             for hand_landmarks in result.multi_hand_landmarks:
-                
+
                 # top
                 thumb_tip = hand_landmarks.landmark[4] 
                 index_tip = hand_landmarks.landmark[8]
@@ -58,32 +62,49 @@ with mp_hands.Hands(
                 middle_mcp = hand_landmarks.landmark[9]
                 ring_mcp = hand_landmarks.landmark[13]
                 pinky_mcp = hand_landmarks.landmark[17]
-                # 2 랑 4 의 거리가 작아야 주먹이된다.
 
                 # 주먹 확인 코드 
-                rock5 = (((thumb_tip.x-index_pip.x)**2) + ((thumb_tip.y-index_pip.y)**2)) * W # 엄지 
-                rock1 = (((index_dip.x-index_mcp.x)**2) + ((index_dip.y-index_mcp.y)**2)) * W # 검지 
-                rock2 = (((middle_dip.x-middle_mcp.x)**2) + ((middle_dip.y-middle_mcp.y)**2)) * W # 중지
-                rock3 = (((ring_dip.x-ring_mcp.x)**2) + ((ring_dip.y-ring_mcp.y)**2)) * W # 약지 
-                rock4 = (((pinky_dip.x-pinky_mcp.x)**2) + ((pinky_dip.y-pinky_mcp.y)**2)) * W # 소지
+                rock5 = get_dis(thumb_tip.x, index_pip.x, thumb_tip.y, index_pip.y, W) # 엄지 
+                rock1 = get_dis(index_dip.x, index_mcp.x, index_dip.y, index_mcp.y, W) # 검지 
+                rock2 = get_dis(middle_dip.x, middle_mcp.x, middle_dip.y, middle_mcp.y, W) # 중지
+                rock3 = get_dis(ring_dip.x, ring_mcp.x, ring_dip.y, ring_mcp.y, W) # 약지 
+                rock4 = get_dis(pinky_dip.x, pinky_mcp.x, pinky_dip.y, pinky_mcp.y, W) # 소지
 
                 if ((((rock1 <= 2) and (rock2 <= 2)) and ((rock3 <= 2) and (rock4 <= 2))) and (rock5 <= 2)):
-                    text = 'ROCK'
+                    text = 'Click'
                     motion(image, text)
+                    temp = index_mcp.x
+                    temp = int(temp * 100)
+                    list.append(temp)
+
                 elif ((((rock1 <= 2) and (rock2 <= 2)) and ((rock3 <= 2) and (rock4 <= 2))) and (rock5 >= 2)):
                     text = 'GOOD'
                     motion(image, text)
+
                 elif ((((rock1 <= 2) and (rock3 <= 2)) and ((rock4 <= 2) and (rock5 <= 2))) and (rock2 >= 2)):
                     text = 'Bad word'
                     motion(image, text)
+
                 else:
                     print_num(image, rock5, rock1, rock2, rock3, rock4)
+                    # value = list[0] - list[-1]
+                    # print(f'이동한 거리 : {value}')
+                    # dis_temp.append(value)
+                    if len(list) > 0 :
+                        value = list[0] - list[-1]
+                    
+                
+                
 
                 mp_drawing.draw_landmarks(
                     image,hand_landmarks, mp_hands.HAND_CONNECTIONS)
                 
+                
+
         cv2.imshow('image', image)
         if cv2.waitKey(1) == ord('q'):
+            print(list)
+            print(value * -1)
             break
 
     cap.release()
